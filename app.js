25d2cf02be6e05cdf62206efc94684eda052da5e
“Nicole <dicocheanicole@gmail.com>
2023-10-11 11:00:40 -0700

    add routes, error handling in app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const { projects } = require('./data.json')
+
+// middleware
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+// routes
+app.get('/', (req, res) => {
+   res.render('index', { projects }) 
+})
+
+app.get('/about', (req, res, next) => {
+    // const err = new Error()
+    // err.status = 500
+    // next(err)
+    res.render('about') 
+})
+
+app.get('/projects/:id', (req, res, next) => {
+    const projectID = req.params.id
+    const project = projects[projectID]
+    if(project) {
+        res.render('project', { project })
+    // handle 404 errors at /project/noroute
+    } else {
+        const err = new Error()
+        err.status = 404
+        err.message =  " Oops. Looks like that project doesn't exist."
+        next(err)
+    }
+})
+
+// error handling
+
+// handle 404 errors at /noroute
+app.use((req, res, next) => {
+    const err = new Error()
+    err.status = 404
+    err.message = "Oops. Looks like that page doesn't exist."
+    next(err)
+})
+// global error
+app.use((err, req, res, next) => {
+    err.status = err.status || 500
+    err.message = err.message || "Oops. Looks like something went wrong on the server." 
+    next(err)
+})
+
+// start server
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`)
+})
